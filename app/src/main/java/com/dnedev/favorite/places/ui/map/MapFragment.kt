b7b36f7e51714d19dab0eb44492d66dd57e275a9
DenislavNedev <dnedev98@gmail.com>
2020-10-29 00:57:24 +0200

    Added google maps fragment

--- app/src/main/java/com/dnedev/favorite/places/ui/map/MapFragment.kt
+++ app/src/main/java/com/dnedev/favorite/places/ui/map/MapFragment.kt
@@ -1,5 +1,7 @@
 package com.dnedev.favorite.places.ui.map
 
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +9,36 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.dnedev.favorite.places.R
 import com.dnedev.favorite.places.databinding.MapFragmentBinding
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.android.support.DaggerFragment
 
-class MapFragment : DaggerFragment() {
+class MapFragment : DaggerFragment(), OnMapReadyCallback {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        //TODO add viewModel.getMarkers
+
+
+        val sydney = LatLng(43.85639, 25.97083)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+
+        //MOVE CAMEREA TO EDI KOI SI
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     private lateinit var binding: MapFragmentBinding
 
@@ -26,5 +55,15 @@ class MapFragment : DaggerFragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("Not yet implemented")
     }
 }
